548a4968ff1a671d3240ed2d119236684dc16c39
Andrew van der Westhuizen <avanderw@gmail.com>
2017-09-04 10:57:13 +0200

    updated kill to finish faster

--- Sample Bots/Java/entelect-battleships/src/main/java/net/avdw/battlefight/kill/KillBehaviourTree.java
+++ Sample Bots/Java/entelect-battleships/src/main/java/net/avdw/battlefight/kill/KillBehaviourTree.java
@@ -10,8 +10,27 @@ public class KillBehaviourTree {
 
     static public Action execute(StateModel stateModel) {
         final StateModel.OpponentCell[][] map = transformMap(stateModel.OpponentMap.Cells);
-        StateModel.OpponentCell killShot = stateModel.OpponentMap.Cells.stream().filter(cell -> cell.Damaged && unfinishedKillCell(map, cell)).findAny().get();
+        StateModel.OpponentCell killShot = stateModel.OpponentMap.Cells.stream().filter(cell -> cell.Damaged && killIsUnfinished(map, cell)).findAny().get();
 
+        printMap(map);
+        return finishKill(map, killShot);
+    }
+
+    private static Action finishKill(StateModel.OpponentCell[][] map, StateModel.OpponentCell killShot) {
+        if (map[killShot.Y - 1][killShot.X].Damaged && !(map[killShot.Y + 1][killShot.X].Missed || map[killShot.Y + 1][killShot.X].Damaged)) {
+            return new KillAction(new Point(killShot.X, killShot.Y + 1));
+        } else if (map[killShot.Y + 1][killShot.X].Damaged && !(map[killShot.Y - 1][killShot.X].Missed || map[killShot.Y - 1][killShot.X].Damaged)) {
+            return new KillAction(new Point(killShot.X, killShot.Y - 1));
+        } else if (map[killShot.Y][killShot.X - 1].Damaged && !(map[killShot.Y][killShot.X + 1].Missed || map[killShot.Y][killShot.X + 1].Damaged)) {
+            return new KillAction(new Point(killShot.X + 1, killShot.Y));
+        } else if (map[killShot.Y][killShot.X + 1].Damaged && !(map[killShot.Y][killShot.X - 1].Missed || map[killShot.Y][killShot.X - 1].Damaged)) {
+            return new KillAction(new Point(killShot.X - 1, killShot.Y));
+        }
+
+        return huntAxis(map, killShot);
+    }
+
+    static Action huntAxis(final StateModel.OpponentCell[][] map, StateModel.OpponentCell killShot) {
         if (!(map[killShot.Y - 1][killShot.X].Damaged || map[killShot.Y - 1][killShot.X].Missed)) {
             return new KillAction(new Point(killShot.X, killShot.Y - 1));
         } else if (!(map[killShot.Y][killShot.X + 1].Damaged || map[killShot.Y][killShot.X + 1].Missed)) {
@@ -25,7 +44,7 @@ public class KillBehaviourTree {
         return new NoAction();
     }
 
-    static boolean unfinishedKillCell(StateModel.OpponentCell[][] map, StateModel.OpponentCell cell) {
+    static boolean killIsUnfinished(StateModel.OpponentCell[][] map, StateModel.OpponentCell cell) {
         if (map[cell.Y - 1][cell.X].Damaged && map[cell.Y + 1][cell.X].Damaged) {
             return false;
         }
@@ -54,6 +73,22 @@ public class KillBehaviourTree {
             map[cell.Y][cell.X] = cell;
         });
         return map;
+    }
+
+    private static void printMap(StateModel.OpponentCell[][] map) {
+        for (int y = 13; y >= 0; y--) {
+            for (int x = 0; x < 14; x++) {
+                if (map[y][x].Damaged) {
+                    System.out.print("!");
+                } else if (map[y][x].Missed) {
+                    System.out.print("x");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println(" " + y);
+        }
+        System.out.println("01234567890123");
     }
 
 }
--- Sample Bots/Java/entelect-battleships/src/test/java/net/avdw/battlefight/kill/KillBehaviourTreeTest.java
+++ Sample Bots/Java/entelect-battleships/src/test/java/net/avdw/battlefight/kill/KillBehaviourTreeTest.java
@@ -14,20 +14,29 @@ import org.junit.Ignore;
 public class KillBehaviourTreeTest {
 
     private static StateModel killState;
+    private static StateModel continueKillState;
 
     @BeforeClass
     public static void setUpClass() throws IOException {
         killState = StateReader.read(new File("src/test/resources/kill-state.json"));
+        continueKillState = StateReader.read(new File("src/test/resources/continue-kill.json"));
     }
 
     @Test
-    public void testExecute() {
+    public void testBasicExecute() {
         Action action = KillBehaviourTree.execute(killState);
         System.out.println(action);
         assertNotEquals("Cannot be an already shot cell", "1,7,3", action.toString());
         assertEquals("First shot is always south cell", "1,7,2", action.toString());
         assertNotEquals(Action.Type.DO_NOTHING, action.type);
         assertNotEquals(Action.Type.SHIELD, action.type);
+    }
+
+    @Test
+    public void testContinueKill() {
+        Action action = KillBehaviourTree.execute(continueKillState);
+        System.out.println(action);
+        assertEquals("Ship is clearly on row 8", 8, action.point.y);
     }
 
     @Test
@@ -47,41 +56,41 @@ public class KillBehaviourTreeTest {
         map[1][1] = shotCell;
         map[1][2] = emptyCell;
         map[0][1] = emptyCell;
-        assertTrue("Surrounded by empty cells is head and tail.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Surrounded by empty cells is head and tail.", KillBehaviourTree.killIsUnfinished(map, shotCell));
 
         map[2][1] = shotCell;
-        assertTrue("Only shot above is unfinished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Only shot above is unfinished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[0][1] = shotCell;
-        assertFalse("Shot above and below is finished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertFalse("Shot above and below is finished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[0][1] = map[2][1] = emptyCell;
 
         map[1][0] = shotCell;
-        assertTrue("Only shot left is unfinished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Only shot left is unfinished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[1][2] = shotCell;
-        assertFalse("Shot left and right is finished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertFalse("Shot left and right is finished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[1][0] = map[1][2] = emptyCell;
 
         map[2][1] = missCell;
-        assertTrue("Only missed above is unfinished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Only missed above is unfinished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[0][1] = map[2][1] = emptyCell;
 
         map[1][0] = missCell;
-        assertTrue("Only missed left is unfinished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Only missed left is unfinished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[1][0] = map[1][2] = emptyCell;
 
         map[2][1] = missCell;
         map[0][1] = shotCell;
-        assertFalse("Missed above and shot below is finished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertFalse("Missed above and shot below is finished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[0][1] = map[2][1] = emptyCell;
 
         map[1][0] = shotCell;
         map[1][2] = missCell;
-        assertFalse("Shot left and missed right is finished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertFalse("Shot left and missed right is finished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[1][0] = map[1][2] = emptyCell;
 
         map[2][1] = shotCell;
         map[1][2] = shotCell;
-        assertTrue("Shot above and right is unfinished.", KillBehaviourTree.unfinishedKillCell(map, shotCell));
+        assertTrue("Shot above and right is unfinished.", KillBehaviourTree.killIsUnfinished(map, shotCell));
         map[2][1] = map[1][2] = emptyCell;
     }
 
